e280d7a81fea88da6c2f649ce545f69b4f41c659
m9oy <mhmdqlan073@gmail.com>
2026-04-28 18:56:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const svgCaptcha = require('svg-captcha');
 
 const app = express();
 
+app.use(cors());
+
+// الصفحة الرئيسية
 app.get('/', (req, res) => {
-  res.send('API works!');
+  res.send('API is working 🚀');
+});
+
+// الكابتشا
+app.get('/captcha', (req, res) => {
+
+  const captcha = svgCaptcha.create();
+
+  res.json({
+    text: captcha.text,
+    svg: captcha.data
+  });
+
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log('Server running');
+});
